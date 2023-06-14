22697f9fbadc1d1ae8e0054d88f9392a3c20de08
Kristofer Raske <kristofer.raske@wustl.edu>
2023-06-14 10:01:32 -0500

    Just following along the tutorial

---
+++ db.js
@@ -0,0 +1,13 @@
+import sqlite3 from "sqlite3";
+
+const filepath = "./lego-database.db";
+
+export default function createDbConnection() {
+  const db = new sqlite3.Database(filepath, (error) => {
+    if (error) {
+      return console.error(error.message);
+    }
+  });
+  console.log("Connection with SQLite has been established");
+  return db;
+}
