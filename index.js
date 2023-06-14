bf40773bcc334a718015ee13bca91391d0e81c16
Kristofer Raske <kristofer.raske@wustl.edu>
2023-06-14 13:47:07 -0500

    Making it so we have an endpoint and the homepage html. Basic stuff
    I forget everything lol

--- index.js
+++ index.js
@@ -7,22 +7,15 @@ const app = express();
 app.use(express.static('static'))
 
 app.get('/data', (req, res) => {
-    res.send('Test Route!')
+    db.all('SELECT * FROM colors LIMIT 10', (err, rows) => {
+        if (err) {
+            throw err;
+        }
+
+        res.json(rows);
+    });
 })
 
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
 });
-// TESTING area
-
-// db.serialize( () => {
-//     db.run('SELECT * FROM colors LIMIT 10')
-// });
-
-db.all('SELECT * FROM colors LIMIT 10', (err, rows) => {
-    if (err) {
-        throw err;
-    }
-
-    console.log(rows);
-})
