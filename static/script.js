dd6ca68319a6116d9920849939b5ae9c3d6dc7c9
Kristofer Raske <kristofer.raske@wustl.edu>
2023-06-15 14:30:10 -0500

    Creating a test area to make sure we have our ajax calls working

--- static/script.js
+++ static/script.js
@@ -2,8 +2,32 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 // DOM ELEMENT STUFF
 let yearInputEl = document.getElementById('year-input');
+let yearButtonEl = document.getElementById('year-submit');
+let svgSectionEl = document.querySelector('.lego-chart-section');
+
+yearButtonEl.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    // Maybe sanitize lol.
+    fetch( '/data/' + yearInputEl.value )
+    .then( (res) => res.json() )
+    .then((data) => {
+        // TODO - here is where we do the 2way binding for the d3 data
+        // For now manual one way binding
+
+        // TODO - TEST section to see if we have this working
+        let testAreaEl = document.querySelector('.test-area');
+        testAreaEl.innerHTML = JSON.stringify(data);
+    });
+});
+
 
 // D3 STUFF
-const div = d3.selectAll("div");
+svgSectionEl.innerHTML = `<svg id="lego-svg" width="50" height="50" font-family="sans-serif" font-size="10" style="display: block;">
+    <text x="0" y="25">SVG is here</text>
+    </svg>
+`
 
-console.log('frontend stuff will go here');
+let svgEl = d3.select('#lego-svg').data();
+
+console.log(svgEl);
