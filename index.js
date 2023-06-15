95d6b378caefaa0dfa5a226f59cb5b882bc33a19
Kristofer Raske <kristofer.raske@wustl.edu>
2023-06-15 09:30:21 -0500

    And there is our REST endpoint yay!

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const app = express();
 app.use(express.static('static'))
 
 app.get('/data/:year', (req, res) => {
-    db.all('SELECT * FROM colors LIMIT 10', (err, rows) => {
+
+    let query = `
+        SELECT  sets.year, colors.rgb, SUM(inventory_parts.quantity) AS quantity
+        FROM sets
+        INNER JOIN inventories ON inventories.set_num = sets.set_num
+        INNER JOIN  inventory_parts ON inventory_parts.inventory_id = inventories.id
+        INNER JOIN colors ON colors.id = inventory_parts.color_id
+        WHERE sets.year = ${req.params.year}
+        GROUP BY colors.rgb
+    `;
+
+    db.all(query, (err, rows) => {
         if (err) {
             throw err;
         }
@@ -20,33 +31,3 @@ app.get('/data/:year', (req, res) => {
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000/');
 });
-
-/**
-
-brick_colors <- sets %>% left_join(inventories, by = "set_num") %>%
-                         left_join(inventory_parts, by = c("id" = "inventory_id")) %>%
-                         left_join(colors, by = c("color_id" = "id")) %>%
-                         select(year, rgb, quantity) %>%
-                         na.omit %>%
-                         group_by(rgb, year) %>%
-                         summarize(total = sum(quantity))
-
-
-Notepad
-
-SELECT  year,  rgb
-FROM sets
-LEFT JOIN inventories ON inventories.set_num = sets.set_num
-LEFT JOIN  inventory_parts ON inventory_parts.inventory_id = inventories.id
-LEFT JOIN colors ON colors.id = inventory_parts.color_id
-
-
-SELECT  sets.year,  colors.rgb, SUM(inventory_parts.quantity) AS quantity
-FROM sets
-LEFT JOIN inventories ON inventories.set_num = sets.set_num
-LEFT JOIN  inventory_parts ON inventory_parts.inventory_id = inventories.id
-LEFT JOIN colors ON colors.id = inventory_parts.color_id
-WHERE sets.year = 2006
-GROUP BY colors.rgb
-
- */
