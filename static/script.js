ebefc7c23d8577f12d3108d5ca50e213dd6c6441
Kristofer Raske <kristofer.raske@wustl.edu>
2023-06-15 14:00:00 -0500

    Ok basic year input. Really basic

--- static/script.js
+++ static/script.js
@@ -1,5 +1,9 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
+// DOM ELEMENT STUFF
+let yearInputEl = document.getElementById('year-input');
+
+// D3 STUFF
 const div = d3.selectAll("div");
 
 console.log('frontend stuff will go here');
